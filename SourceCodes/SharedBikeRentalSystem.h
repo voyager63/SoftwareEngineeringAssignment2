ba9e32dbf099347d4b82b1f2f33d8afb84ca386d
voyager63 <studylee63@gmail.com>
2025-05-22 23:12:28 +0900

    Header File Update

--- SourceCodes/SharedBikeRentalSystem.h
+++ SourceCodes/SharedBikeRentalSystem.h
@@ -4,34 +4,126 @@
 using namespace std;
 
 class User {
-
+private:
+	string ID;
+	string passWord;
+	string phoneNumber;
+	char userType;
+	RentedBikeCollection* ownedRentedBikeCollection;
+public:
+	string getPassword();
+	RentedBikeCollection* getUserBikes();
+	bool isAdmin();
+	bool isMember();
 };
 
 class UserCollection {
-
+private:
+	User* userList[50];
+	int numUsers;
+public:
+	void addNewUser();
+	User* getUserByID(string id);
 };
 
 class Bike {
-
+private:
+	string bikeID;
+	string bikeProductName;
+public:
+	string getBikeDetails();
 };
 
 class BikeCollection {
-
+private:
+	Bike* bikeList[50];
+	int numBikes;
+public:
+	void addNewBike();
+	Bike* getBikeByID(string bikeID);
 };
 
 class RentedBikeCollection {
-
+private:
+	Bike* ownedRentedBike[50];
+public:
+	void addNewBike();
 };
 
 class AccessManager {
-
+private:
+	User* currentUser;
+public:
+	void connect();
+	void disconnect();
+	User* getCurrentUser();
 };
 
-class SignUpUI {
 
+class SignUpUI {
+public:
+	void startInterface();
+	void createNewUser();
 };
 
 class SignUp {
+public:
+	void addNewUser();
+};
+
+class LoginUI{
+public:
+	void startInterface();
+	void clickLogin();
+};
+
+class Login {
+public:
+	void checkValid();
+	bool isEqual();
+};
+
+class LogoutUI {
+public:
+	void startInterface();
+	void clickLogout();
+};
+
+class Logout {
+public:
+	void exitSystemAccess();
+};
+
+class RegisterBikeUI {
+public:
+	void startInterface();
+	void registerNewBike();
+};
+
+class RegisterBike {
+public:
+	void createNewBike();
+};
+
+class RentBikeUI {
+public:
+	void startInterface();
+	void rentBike();
 
 };
 
+class RentBike {
+public:
+	void addNewBike();
+};
+
+class CheckBikeRentalInformationUI {
+public:
+	void startInterface();
+	void checkBikeRentalInformation();
+};
+
+class CheckBikeRentalInformation {
+public:
+	void showBikeRentalInformation();
+};
