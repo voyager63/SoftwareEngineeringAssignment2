a2b8ad71bc6da3ceb612a182933e4d4838026efe
voyager63 <studylee63@gmail.com>
2025-05-22 21:23:41 +0900

    SourceCodes Add

---
+++ SourceCodes/SharedBikeRentalSystem.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class User {
+
+};
+
+class UserCollection {
+
+};
+
+class Bike {
+
+};
+
+class BikeCollection {
+
+};
+
+class RentedBikeCollection {
+
+};
+
+class AccessManager {
+
+};
+
+class SignUpUI {
+
+};
+
+class SignUp {
+
+};
+
---
+++ SourceCodes/main.cpp
@@ -0,0 +1,5 @@
+#include "SharedBikeRentalSystem.h"
+
+int main(){
+
+}
